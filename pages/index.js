3ebc9fac866cfd59b855755ae7cfb9b8a68bdb8d
sumanth <sumanth_nathella@homedepot.com>
2021-01-31 00:17:45 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,164 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="p-8 shadow-lg">
+      <img className="rounded text-center" src={props.img} alt={props.imgAlt} />
+      <div className="mt-2">
+        <div>
+          <div className="text-xl text-gray-600 text-center text-bold font-sans">
+            {props.eyebrow}
+          </div>
+          <div className="text-justify">{props.title}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div
+        className="my-6 grid grid-cols-6 gap-4 bg-fixed font-sans"
+        style={{
+          backgroundImage: `url(/ipadbg.png)`,
+          backgroundPosition: "-500px -200px",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="col-span-1"></div>
+        <div className=" col-span-4 my-12 py-8 text-white">
+          <div className="py-2 text-4xl text-bold">IPitch</div>
+          <div className="text-3xl">
+            An application which helps to increase productivity for sales team
+          </div>
         </div>
-      </main>
+        <div className="col-span-1"></div>
+      </div>
+      <div className="grid grid-cols-6 gap-4 font-serif">
+        <div className="col-span-1"></div>
+        <div className="col-span-4 text-lg">
+          <div className="">
+            One of the top 100 pharma companies approached Cognizant, who’s a
+            leading IT Software Development, to help them to solve the issues
+            faced by their sales team. By building a digital solution that could
+            provide a 360° view on their business and team members performance.
+          </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          <div className="my-4 text-2xl font-bold font-sans text-gray-900">
+            Business Problem
+          </div>
+          <div>
+            <div>- Losing the market share.</div>
+            <div>- Decreasing productivity from the sales team.</div>
+            <div>- No new business.</div>
+            <div>- Losing credibility from the existing customers</div>
+            <div>
+              - Collaboration is missing between cross functional teams.
+            </div>
+            <div>- Unable to meet any reasonable annual targets.</div>
+            <div>- Unable to perform on par with the competitors.</div>
+          </div>
+
+          <div className="my-4 text-2xl font-bold font-sans text-gray-900">
+            What’s my role?
+          </div>
+          <div>
+            Being a lead UX consultant, specialised in Product Strategy, Design
+            and UX. My role was to apply Human Centered Design methods to a
+            project with immense technical focus and to provide valuable
+            insights from user research to help build a solution that would take
+            into account the complexity of everyday business cases and real user
+            needs.
+          </div>
+        </div>
+        <div className="col-span-1"></div>
+      </div>
+      <div className="my-6 col-auto bg-blue-500 text-center font-sans">
+        <div className="p-8 text-7xl text-white">
+          Impact and Results Achieved
+        </div>
+      </div>
+      <div className="grid grid-cols-6 gap-4 font-serif">
+        <div className="col-span-1"></div>
+        <div className="col-span-4 text-lg">
+          <ul style={{ listStyleType: "disc" }}>
+            <li>
+              {" "}
+              Sales team productivity increased to 60% by using iPitch
+              application.
+            </li>
+            <li> New customers increased to 40% </li>
+            <li> 60% of sales revenue increased</li>
+            <li> 80% of operational cost reduced</li>
+            <li>50% of IT cost reduced</li>
+            <li>
+              {" "}
+              From 4 different applications, users have come to use a single
+              application
+            </li>
+          </ul>
+          <div className="flex flex-row flex-wrap justify-between my-5">
+            <div className="max-w-1/4 m-2">
+              <Card
+                img="icon-copy.png"
+                imgAlt="Research"
+                eyebrow="Research"
+                title="Gain user insight and understand challenges. Look for ideas"
+                pricing="..."
+              />
+            </div>
+            <div className="max-w-1/4">
+              <Card
+                img="icon-copy.png"
+                imgAlt="Research"
+                eyebrow="Research"
+                title="Gain user insight and understand challenges. Look for ideas"
+                pricing="..."
+              />
+            </div>
+            <div className="max-w-1/4">
+              <Card
+                img="icon-copy.png"
+                imgAlt="Research"
+                eyebrow="Research"
+                title="Gain user insight and understand challenges. Look for ideas"
+                pricing="..."
+              />
+            </div>
+            <div className="max-w-1/4">
+              <Card
+                img="icon-copy.png"
+                imgAlt="Research"
+                eyebrow="Research"
+                title="Gain user insight and understand challenges. Look for ideas"
+                pricing="..."
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1"></div>
+      </div>
+      <footer>
+        <div className="m-4 font-sans text-center">
+          Rajesh Revoor designs<span>&#174;</span>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,92 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      white: colors.white,
+      gray: {
+        100: "#f7fafc",
+        900: "#1a202c",
+        DEFAULT: "#1a202c",
+      },
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    spacing: {
+      px: "1px",
+      0: "0",
+      0.5: "0.125rem",
+      1: "0.25rem",
+      1.5: "0.375rem",
+      2: "0.5rem",
+      2.5: "0.625rem",
+      3: "0.75rem",
+      3.5: "0.875rem",
+      4: "1rem",
+      5: "1.25rem",
+      6: "1.5rem",
+      7: "1.75rem",
+      8: "2rem",
+      9: "2.25rem",
+      10: "2.5rem",
+      11: "2.75rem",
+      12: "3rem",
+      14: "3.5rem",
+      16: "4rem",
+      20: "5rem",
+      24: "6rem",
+      28: "7rem",
+      32: "8rem",
+      36: "9rem",
+      40: "10rem",
+      44: "11rem",
+      48: "12rem",
+      52: "13rem",
+      56: "14rem",
+      60: "15rem",
+      64: "16rem",
+      72: "18rem",
+      80: "20rem",
+      96: "24rem",
+    },
+    borderRadius: {
+      none: "0",
+      sm: ".125rem",
+      DEFAULT: ".25rem",
+      lg: ".5rem",
+      full: "9999px",
+    },
+    fontFamily: {
+      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+      serif: ["Roboto", ...defaultTheme.fontFamily.serif],
+    },
+    listStyleType: {
+      disc: 'disc',
+      hyphen: '-'
+    },
+    maxWidth: {
+      '1/4': '20%',
+      '1/2': '50%',
+      '3/4': '75%',
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
