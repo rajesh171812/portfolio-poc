5e221d4bf3b800b7838d17c77919aaff0a526973
sumanth <sumanth_nathella@homedepot.com>
2021-02-02 21:24:35 -0500

    feat: fix import file case

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import App from 'next/app';
-import Layout from '../components/Layout';
-import '../styles/globals.css'
+import React from "react";
+import App from "next/app";
+import Layout from "../components/layout";
+import "../styles/globals.css";
 
 export default class MyApp extends App {
-  render () {
-    const { Component, pageProps } = this.props
+  render() {
+    const { Component, pageProps } = this.props;
     return (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-    )
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
   }
 }
-
