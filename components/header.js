7cf55b387d9b0b2b033738704509283c59e3b279
sumanth <sumanth_nathella@homedepot.com>
2021-02-03 23:07:28 -0500

    style: fix menu and add mobile styles for hamburger

--- components/header.js
+++ components/header.js
@@ -3,27 +3,42 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex flex-row justify-center p-8 border-b-2 border-white font-sans">
-      <div className="mx-4 text-xl text-white">
-        <Link href="/work">
-          <div className="text-sans cursor-pointer">Work</div>
-        </Link>
+    <div className="p-4 md:p-8 border-b-2 border-white font-sans">
+      <div className="hidden md:flex flex-row justify-center ">
+        <div className="mt-3 mr-20 w-72 border-t-2 border-white"></div>
+        <div className="mx-4 text-xl text-white">
+          <Link href="/work">
+            <div className="text-sans cursor-pointer uppercase">Work</div>
+          </Link>
+        </div>
+        <div className="mx-4 text-xl text-white">
+          <Link href="/about">
+            <div className="text-sans cursor-pointer uppercase">About</div>
+          </Link>
+        </div>
+        <div className="mx-4 text-xl text-white">
+          <Link href="/contact">
+            <div className="text-sans cursor-pointer uppercase">Contact</div>
+          </Link>
+        </div>
+        <div className="mx-4 text-xl text-white">
+          <Link href="/resume">
+            <div className="text-sans cursor-pointer uppercase">Resume</div>
+          </Link>
+        </div>
+        <div className="mt-3 ml-20 w-72 border-t-2 border-white "></div>
       </div>
-      <div className="mx-4 text-xl text-white">
-        <Link href="/about">
-          <div className="text-sans cursor-pointer">About</div>
-        </Link>
+      <div className="md:hidden flex justify-between font-sans">
+        <div className="mx-4 text-xl text-white">Rajesh Revoor</div>
+        <div className="nav-toggle-btn flex flex-col">
+          <span className="w-6 mb-1.5 border-t-2 border-white"></span>
+          <span className="w-6 mb-1.5 border-t-2 border-white"></span>
+          <span className="w-6 mb-1.5 border-t-2 border-white"></span>
+          <span className="w-6 mb-1.5 border-t-2 border-white"></span>
+
+        </div>
       </div>
-      <div className="mx-4 text-xl text-white">
-        <Link href="/contact">
-          <div className="text-sans cursor-pointer">Contact</div>
-        </Link>
-      </div>
-      <div className="mx-4 text-xl text-white">
-        <Link href="/resume">
-          <div className="text-sans cursor-pointer">Resume</div>
-        </Link>
-      </div>
+
     </div>
   );
 }
--- components/scrollToTop.js
+++ components/scrollToTop.js
@@ -5,7 +5,7 @@ export default function ScrollToTop(props) {
         window.scrollTo({top: 0, behavior: 'smooth'});
     };
     return (
-        <div onClick={scrollTop} className="fixed bottom-20 right-10 bg-black hover:bg-blue-700 w-16 h-16 flex items-center justify-center text-white text-2xl cursor-pointer ">
+        <div onClick={scrollTop} className="fixed bottom-20 right-10 bg-black hover:bg-blue-700 w-16 h-16 flex items-center justify-center text-white text-2xl cursor-pointer z-50">
             <span>&#x5e;</span>
         </div>
     );
